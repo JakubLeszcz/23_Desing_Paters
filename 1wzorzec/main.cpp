0f953576c89553a1c9e5577286fc7a0f3110c9eb
JakubLeszcz <63248667+JakubLeszcz@users.noreply.github.com>
2021-05-12 23:44:47 +0200

    Add files via upload

--- 1wzorzec/main.cpp
+++ 1wzorzec/main.cpp
@@ -3,42 +3,67 @@
 #include <string>
 using namespace std;
 
+class CalculatePoints {
+public:
+
+    void calculatePoints(){
+        cout<<"obliczanie punktów"<<endl;
+        checkIfEnd();
+    };
+    void checkIfEnd(){
+        cout<<"sprawdz czy koniec"<<endl;
+    };
+
+};
+class CheckingDartPostion{
+public:
+    void checkDartPositon(string rzuty){
+        cout<<"Obliczanie pozycji "<<endl;
+
+    };
+};
+
+class GameFacade {
+public:
+    GameFacade(){
+    }
+
+    void getPointsAfterTrow(string rzuty){
+
+        getPositon(rzuty);
+        getPoints();
+    }
+private:
+    void getPositon(string rzuty) {
+        CheckingDartPostion cdp;
+        cdp.checkDartPositon(rzuty);
+
+    }
+    void getPoints() {
+        CalculatePoints cp;
+        cp.calculatePoints();
+    }
+};
+
 class Client {
 public:
     void throwDarts() {
         cout << "Pierwszy rzut" << endl;
         cout << "Drugi rzut" << endl;
         cout << "Trzeci rzut" << endl;
-    }
+        string rzuty;
+        GameFacade gf;
+        gf.getPointsAfterTrow(rzuty);
+    };
+    Client(){};
 
 private:
     string playerName;
     int points;
 };
-class GameFacade {
-public:
-    void getPointsAfterTrow(){
-
-    }
-private:
-    void getPositon() {
-    }
-    void getPoints() {
-    }
-};
-class CheckingDartPostion{
-private:
-    void checkDartPositon();
-};
-class CalculatePoints {
-private:
-    GameFacade game;
-    Client player;
-    void calculatePoints();
-    void checkIfEnd();
-};
 int main() {
-
+    Client client = Client();
+    client.throwDarts();
     return 0;
 }
 
